be4c29072f4e9e165410aa193025c84be4d428f0
sajidhussain420 <50439976+sajidhussain420@users.noreply.github.com>
2019-05-09 12:17:54 +0500

    Git commits

---
+++ Ladder.java
@@ -0,0 +1,18 @@
+// Demonstrate an if-else-if ladder.
+class Ladder {
+	public static void main(String args[]) {
+	  int x;
+	  for(x=0 ; x<6 ; x++) {
+	    if(x==1)
+		System.out.println("x is one");
+	    else if(x==2)
+		System.out.println("x is two");
+	    else if(x==3)
+		System.out.println("x is three");
+	    else if(x==4)
+		System.out.println("x is four");
+            else
+		system.out.println("x is not between 1 and 5");
+	  }
+     } 
+}
